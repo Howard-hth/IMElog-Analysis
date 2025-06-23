44aa0ded4090953a3695023e07a3e251ee2e2f7d
Howard-hth <tinghaokang@gmail.com>
2025-06-23 10:39:08 +0800

    Update parser.ps1

--- parser.ps1
+++ parser.ps1
@@ -6,9 +6,9 @@ $logContents = Get-Content -Path $fullLogPath # type: Object[]
 
 # Initial Line: <![LOG[[ConfigUpdate] Received configuration JSON: {"key":"value","id":7,"active":false}]LOG]!><time="23:05:09.000039" date="4-16-2023" component="ConfigManager" context="Deploy" type="3" thread="99" file="data.json">
 # Target Output: 
-## Title: "Message", "time", "date", "component", "context", "type", "thread", "file"
+## Title: "Message","time","date","component","context","type","thread","file"
 ## Content: "Received configuration JSON: {'key':'value','id':7,'active':false}", "23:05:09.000039", "4-16-2023", "ConfigManager", "Deploy", "3", "99", "data.json"
-$csvTitle = '"Message", "time", "date", "component", "context", "type", "thread", "file"'
+$csvTitle = '"Message","time","date","component","context","type","thread","file"'
 $csvContent = @($csvTitle)
 $partialLines = ""
 $replaceRefs = @(
